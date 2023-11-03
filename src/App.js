1fa67fe0290e6a04f6ccd9e2e81df3a5643a327d
CodeSplash7 <codesplash7@gmail.com>
2023-11-03 20:45:07 +0200

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<h1>Hello</h1>}>
+      <Route path="home" element={<p>Hi</p>} />
+    </Route>
+  )
+);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
