dc4941e6027fcced17b0cb8ad33cab47c3887d1b
CodeSplash7 <codesplash7@gmail.com>
2023-11-17 19:09:15 +0200

    no bugs found so i'll push it to production, commented the code and cleaned it a little

--- src/components/MoviesPage/MovieFilter.jsx
+++ src/components/MoviesPage/MovieFilter.jsx
@@ -3,38 +3,36 @@ import { capitalize } from "../../helperFunctions";
 function MovieFilter({ currentFilter, genres, filterMovies, valueOfNoFilter }) {
   return (
     <>
-      <div
-        className={`movies-page__filter-btn ${
-          currentFilter == valueOfNoFilter && "movies-page__filter-btn--active"
-        }`}
-        onClick={() => filterMovies(valueOfNoFilter)}
-      >
-        <div className="movies-page__filter-content">
-          <p className="movies-page__filter-genre">
-            {capitalize(valueOfNoFilter)}
-          </p>
-          <div className="movies-page__filter-underline"></div>
-        </div>
-      </div>
+      <FilterButton currentFilter={currentFilter} genre={valueOfNoFilter} />
+
       {genres.map((genre, i) => {
         return (
-          <div
+          <FilterButton
             key={i}
-            onClick={() => filterMovies(genre.name)}
-            className={`movies-page__filter-btn ${
-              currentFilter == genre.name && "movies-page__filter-btn--active"
-            }`}
-          >
-            <div className="movies-page__filter-content">
-              <p className="movies-page__filter-genre">
-                {capitalize(genre.name)}
-              </p>
-              <div className="movies-page__filter-underline"></div>
-            </div>
-          </div>
+            currentFilter={currentFilter}
+            genre={genre.name}
+            filterMovies={filterMovies}
+          />
         );
       })}
     </>
+  );
+}
+
+function FilterButton({ currentFilter, genre, filterMovies }) {
+  return (
+    <div
+      onClick={() => filterMovies(genre)}
+      // adding the main class and the active class if it's the case
+      className={`movies-page__filter-btn ${
+        currentFilter == genre && "movies-page__filter-btn--active"
+      }`}
+    >
+      <div className="movies-page__filter-content">
+        <p className="movies-page__filter-genre">{capitalize(genre)}</p>
+        <div className="movies-page__filter-underline"></div>
+      </div>
+    </div>
   );
 }
 
